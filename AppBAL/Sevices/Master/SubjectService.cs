ce89bda8ffb7d888dbdc64b66530e4217efd2754
Ashoke <talk2ashoke@gmail.com>
2020-09-23 22:11:22 +0530

    Subject Service
    Subject Service started

---
+++ AppBAL/Sevices/Master/SubjectService.cs
@@ -0,0 +1,34 @@
+﻿using AppDAL.DBRepository;
+using AppModel;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBAL.Sevices
+{
+    public interface ISubjectService
+    {
+        Task<CommonResponce> GetAllSubjects();
+    }
+
+    public class SubjectService:ISubjectService
+    {
+        private readonly ISubjectRepository _DBSubjectRepository;
+        private readonly IMapper _mapper;
+        
+        public SubjectService(ISubjectRepository DBSubjectRepository,IMapper mapper)
+        {
+            _DBSubjectRepository = DBSubjectRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CommonResponce> GetAllSubjects()
+        {
+            var AllSubjects = await _DBSubjectRepository.GetAllSubjects();
+            CommonResponce result = new CommonResponce { Stat = true, StatusMsg = "", StatusObj = AllSubjects };
+            return result;
+        }
+    }
+}
---
+++ AppDAL/DBRepository/Master/SubjectRepository.cs
@@ -0,0 +1,42 @@
+﻿using AppDAL.DBModels;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDAL.DBRepository
+{
+    public interface ISubjectRepository
+    {
+        Task<List<Tblmsubject>> GetAllSubjects();
+        Task<Tblmsubject> GetSubjectBySubjectId(int SubjectID);
+        Task<Tblmsubject> GetSubjectBySubjectName(string SubjectName);
+    }
+
+    public class SubjectRepository:ISubjectRepository
+    {
+        private readonly AppDBContext _DBContext;
+        public SubjectRepository(AppDBContext DBContext)
+        {
+            _DBContext = DBContext;
+        }
+
+        public async Task<List<Tblmsubject>> GetAllSubjects()
+        {
+            var result = await _DBContext.Tblmsubject.ToListAsync();
+            return result;
+        }
+
+        public async Task<Tblmsubject> GetSubjectBySubjectId(int SubjectID)
+        {
+            var oSubject = await _DBContext.Tblmsubject.Where(s => s.Id.Equals(SubjectID)).FirstOrDefaultAsync();
+            return oSubject;
+        }
+
+        public async Task<Tblmsubject> GetSubjectBySubjectName(string SubjectName)
+        {
+            var oSubject = await _DBContext.Tblmsubject.Where(s => s.Name.Equals(SubjectName)).FirstOrDefaultAsync();
+            return oSubject;
+        }
+    }
+}
